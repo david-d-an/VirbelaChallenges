f4906fb30e6e0b6bdaa7261993c26f3a801d9aa4
David An <dong82@gmail.com>
2021-04-01 22:20:32 -0400

    Testing environment setup

---
+++ Exercise1/Exercise1.Api.Test/Controllers/UserControllerTest.cs
@@ -0,0 +1,28 @@
+using System.Collections.Generic;
+using System.Linq;
+using Exercise1.Data.Repos;
+using Exercise1.Data.Models.VirbelaListing;
+using Moq;
+using Xunit;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace Exercise1.Api.Controllers
+{
+    public class UserControllerTest {
+        public UserControllerTest()
+        {
+            
+        }
+
+        [Fact]
+        public void SampleTest() {
+            // Arrange
+
+            // Act
+
+            // Assert
+
+        }
+    }
+}
