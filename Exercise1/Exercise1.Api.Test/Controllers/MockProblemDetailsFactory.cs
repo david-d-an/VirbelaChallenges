8e7b5f11a247686dc5043c2f45b4459580d442e0
David An <dong82@gmail.com>
2021-04-03 17:50:32 -0400

    MockProblemDetailsFactory added to enable testing on Problem() response type

---
+++ Exercise1/Exercise1.Api.Test/Controllers/MockProblemDetailsFactory.cs
@@ -0,0 +1,47 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.Collections.Generic;
+
+namespace Exercise1.Api.Controllers
+{
+    public class MockProblemDetailsFactory : ProblemDetailsFactory
+    {
+        public MockProblemDetailsFactory() { }
+
+        public override ProblemDetails CreateProblemDetails(HttpContext httpContext,
+            int? statusCode = default, 
+            string title = default,
+            string type = default, 
+            string detail = default, 
+            string instance = default)
+        {
+            return new ProblemDetails() {
+                Detail = detail,
+                Instance = instance,
+                Status = statusCode,
+                Title = title,
+                Type = type,
+            };
+        }
+
+        public override ValidationProblemDetails CreateValidationProblemDetails(
+            HttpContext httpContext,
+            ModelStateDictionary modelStateDictionary, 
+            int? statusCode = default,
+            string title = default, 
+            string type = default, 
+            string detail = default,
+            string instance = default)
+        {
+            return new ValidationProblemDetails(new Dictionary<string, string[]>()) {
+                Detail = detail,
+                Instance = instance,
+                Status = statusCode,
+                Title = title,
+                Type = type,
+            };
+        }
+    }
+}
